3ee1ea2729656cbb3c986128bc2560b590cb701f
sadayuki-matsuno <sadayuki.matsuno@gmail.com>
2019-02-02 13:53:31 +0900

    first

---
+++ app/src/main/java/com/example/study_kotiln/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.study_kotiln
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
