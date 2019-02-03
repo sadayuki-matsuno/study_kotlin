a70a62c87d9a76944d521620e1a0795afa3fdd7d
sadayuki-matsuno <sadayuki.matsuno@gmail.com>
2019-02-03 15:24:53 +0900

    add data class

--- app/src/main/java/com/example/study_kotiln/Fetch.kt
+++ app/src/main/java/com/example/study_kotiln/Fetch.kt
@@ -1,7 +1,10 @@
 package com.example.study_kotiln
 
+import com.beust.klaxon.Klaxon
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.result.Result
+
+class Person (val name: String, var age: Int = 23)
 
 class Fetch {
     fun fetch(url: String) {
@@ -10,7 +13,10 @@ class Fetch {
             when (result) {
                 is Result.Success -> {
                     // レスポンスボディを表示
-                    println("非同期処理の結果：" + String(response.data))
+                    println(String(response.data))
+                    val result = Klaxon().parseArray<Repo>(String(response.data))
+                    println("非同期処理の結果：")
+                    println(Klaxon().toJsonString(result))
                 }
                 is Result.Failure -> {
                     println(request)
---
+++ app/src/main/java/com/example/study_kotiln/GithubModel.kt
@@ -0,0 +1,23 @@
+package com.example.study_kotiln
+
+import com.beust.klaxon.Json
+
+data class Repo (
+    val id : Int,
+    val name: String,
+    @Json(name = "full_name") val fullName: String,
+    val private: Boolean,
+    val owner: Owner,
+    val fork: Boolean,
+    val size: Int? = 0,
+    @Json(name = "stargazers_count") val star: Int = 0,
+    @Json(name = "open_issues") val openIssues: Int = 0
+)
+
+data class Owner (
+    val login : String,
+    val id: Int,
+    @Json(name = "avatar_url") val avatarUrl: String = "",
+    val url: String,
+    val type: String
+)
